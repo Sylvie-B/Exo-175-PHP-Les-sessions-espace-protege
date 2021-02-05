545fe9a3f5497331f9a50dfa66a5157d328a3933
Sylvie-B <unsolo@hotmail.fr>
2021-02-05 09:56:47 +0100

    si mot passe ouvre la page

--- index.php
+++ index.php
@@ -17,12 +17,18 @@ session_start();
     <form method="post" action="page1.php">
 
         <label for="passWord">mot de passe</label>
-        <input type="password" id="passWord">
+        <input type="password" id="passWord" name="pass">
         <button type="submit">valider</button>
 
     </form>
 
-
 </body>
 </html>
 
+<?php
+
+if(isset($_POST['error']) && $_POST['error'] === 1){
+    echo "mot de pass incorrect";
+}
+
+?>
